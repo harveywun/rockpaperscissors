66b9d5414455a7e0def87431d15d43f274b99d09
Harvey Wun <harvey.wun@gmail.com>
2021-12-17 15:44:35 +0800

    added buttons

--- rps.js
+++ rps.js
@@ -1,6 +1,8 @@
 console.log("hello world")
 let youWin = 0;
-let computerWin = 0;
+let youLose = 0;
+let draws = 0;
+let gamesPlayed = 0;
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -15,8 +17,10 @@ function playRound(playerSelection, computerSelection) {
 
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
+    gamesPlayed++;
 
     if (playerSelection == computerSelection) {
+        draws++;
         return "You draw!";
     }
 
@@ -25,6 +29,7 @@ function playRound(playerSelection, computerSelection) {
             youWin++;
             return "You win! Rock beats scissors";
         } else {
+            youLose++;
             return "You lose. Paper beats rock";
         }
     }
@@ -34,6 +39,7 @@ function playRound(playerSelection, computerSelection) {
             youWin++;
             return "You win! Paper beats rock";
         } else {
+            youLose++;
             return "You lose. Scissors beats paper";
         }
     }
@@ -42,6 +48,7 @@ function playRound(playerSelection, computerSelection) {
             youWin++;
             return "You win! Scissors beats paper";
         } else {
+            youLose++;
             return "You lose. Rock beats scissors";
         }
     }
@@ -49,10 +56,30 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-    for(let i = 0; i < 5; i++) {
-        let playerSelection = prompt("You play?", "eg. rock");
-        let computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection));
-    }
+    let playerSelection = prompt("You play?", "eg. rock");
+    let computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+
     console.log(`You won ${youWin} games`);
 }
+
+const buttons = document.querySelectorAll('button');
+const result = document.querySelector('#result');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let computerSelection = computerPlay();
+        console.log(button.value);
+
+        result.innerHTML = '<div>' + playRound(button.value, computerSelection) + '</div>';
+        result.innerHTML += `<div>Wins: ${youWin} Losses: ${youLose} Draws: ${draws} </div>`;
+    
+        if (gamesPlayed == 5) {
+            result.innerHTML += `<div>You have played 5 games.  ${youWin > youLose ? 'You won!' : 'You did not win.'} </div>`;
+            youWin = 0;
+            youLose = 0;
+            draws = 0;
+            gamesPlayed = 0;
+        }
+    });
+});
